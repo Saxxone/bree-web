5fbfa3e97a88d35c1a89691b90cbb7214034fd28
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2024-11-07 13:35:27 +0000

    fix(app): home spacing on home bottom

--- pages/home.vue
+++ pages/home.vue
@@ -22,7 +22,7 @@ const { reset } = useInfiniteScroll(
     // current_page.value++;
     // await useDynamicScroll(scroll_element.value as HTMLElement, getFeed);
   },
-  { distance: 10000000 },
+  { distance: 10000000 }
 );
 
 async function fetchFeed() {
@@ -42,11 +42,8 @@ onBeforeMount(async () => {
 <template>
   <div>
     <div ref="scroll_element">
-      <PostsSocialPost
-        v-for="post in postsStore.feed"
-        :key="post.id"
-        :post="post"
-      />
+      <PostsSocialPost v-for="post in postsStore.feed" :key="post.id" :post="post" />
     </div>
+    <AppSpacerY size="md" />
   </div>
 </template>
