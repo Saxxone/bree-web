42617889a268b14b8ccd19cfcefccbf3eff83cca
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2024-11-08 16:35:57 +0000

    feat(deployment): deployment config with pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: "bree-web",
+      script: "npm run preview",
+      cwd: "./",
+
+      env: {
+        NODE_ENV: "production",
+      },
+
+      // Process management
+      instances: 1,
+      exec_mode: "cluster",
+      autorestart: true,
+      watch: false,
+
+      // Logging
+      out_file: "./logs/out.log",
+      error_file: "./logs/error.log",
+
+      max_memory_restart: "1G", // Restart if memory usage exceeds 1GB
+    },
+  ],
+};
