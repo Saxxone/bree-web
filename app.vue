a1a485f16fcb1432f37e92949b97dde424bf79e7
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2024-12-05 21:06:23 +0100

    feat(analytics): added gtag (#16)

--- app.vue
+++ app.vue
@@ -2,6 +2,20 @@
 import { useGlobalStore } from "~/store/global";
 const globalStore = useGlobalStore();
 const { closeSnack } = globalStore;
+
+onMounted(() => {
+  //@ts-expect-error gtag is loaded externally
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    //@ts-expect-error gtag is loaded externally
+    dataLayer.push(arguments);
+  }
+  //@ts-expect-error gtag is loaded externally
+  gtag("js", new Date());
+
+  //@ts-expect-error gtag is loaded externally
+  gtag("config", "G-9SMJ6QLH4J");
+});
 </script>
 
 <template>
