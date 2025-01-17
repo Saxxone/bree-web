a50bb1197f230435f5b0dce0b6444d79a738b543
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2025-01-17 21:12:18 +0000

    feat(app): added infinite scrollobserver

---
+++ components/app/AppInfiniteScroll.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+
+const emit = defineEmit(['intersected'])
+const target = ref<Element | null>(null)
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.1,
+};
+const observer = ref<IntersectionObserver| null>(null)
+
+function handleIntersection(entries: any) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      emit('intersected')
+    }
+  });
+}
+
+
+onMounted(async () => {
+  target.value = document.querySelector('#bottom-of-page-observable')
+  observer.value = new IntersectionObserver(handleIntersection, options);
+  if (target.value) {
+    observer.value.observe(target.value);
+  }
+});
+
+</script>
+
+
+<template>
+<div id="bottom-of-page-observable"></div>
+</template>
